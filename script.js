e83d2ad87e6c612b9e25c7fd4829702f467acb79
Андрій Сопов <asopov2006@mail.com>
2025-11-06 14:36:13 +0200

    perf: оптимізовано анімацію скролу з rootMargin

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const faders = document.querySelectorAll('section');
-const appearOptions = { threshold: 0.2 };
+const appearOptions = { threshold: 0.2, rootMargin: '0px 0px -50px 0px' };
+const faders = document.querySelectorAll('section, footer');
 
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
     entries.forEach(entry => {
